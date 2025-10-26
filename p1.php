f48a3cc133b0e815964ef72de35293ccb323b644
Syed Al Mahmud <mahmud.agni@gmail.com>
2025-10-26 12:16:41 +0600

    Create p1.php

---
+++ p1.php
@@ -0,0 +1,41 @@
+<DOCTYPE html>
+
+<head>
+
+<title> WebTech</title>
+
+</head>
+
+<body>
+<center>
+    <h1>Welcome to the first lab</h1>
+    <h2>Lets make a reg form </h2>
+</center>
+
+Eenter Your Name:
+<input type="text"> <br><br>
+
+Enter Your Age:
+<input type="text"> <br><br>
+
+Enter Your DOB:
+<input type="date"> <br><br>
+
+Enter Your Pref Lang:
+<input type="radio"> Bangla
+<input type="radio"> English
+<input type="radio"> Hindi
+
+<br><br>
+
+Enter your Pref Lang:
+<input type="checkbox" name="" id= "">Arabic
+<input type="checkbox" name="" id= "">French
+
+<br><br>
+
+<center>
+    <input type ="Submit">
+</center>
+
+</body>
