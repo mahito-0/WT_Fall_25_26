3ef25fd277fc6350d68cefbce147a1fbd7d2098c
Syed Al Mahmud <mahmud.agni@gmail.com>
2025-10-26 12:33:19 +0600

    Create aiub.php

---
+++ aiub.php
@@ -0,0 +1,67 @@
+<DOCTYPE html>
+
+<head>
+
+<title> WebTech</title>
+
+</head>
+
+<body>
+<center>
+    <h1>AIUB Management System</h1>
+    <h2>We Create Future</h2>
+</center>
+
+<h1>Registration Form</h1>
+
+First Name:
+<input type="text"> <br><br>
+
+Last Name:
+<input type="text"> <br><br>
+
+Age:
+<input type="date"> <br><br>
+
+designation:
+<input type="radio"> Junior Position
+<input type="radio"> Senior Position
+<input type="radio"> Project Lead
+<br><br>
+
+Preferred Language:
+<input type="checkbox" name="" id= "">JAVA
+<input type="checkbox" name="" id= "">C++
+<input type="checkbox" name="" id= "">PHP
+<br><br>
+
+preferred course:
+<select>
+    <option>Web Technology</option>
+    <option>Database Management System</option>
+    <option>Data Science</option>
+    <option>Artificial Intelligence</option>
+</select>
+<br><br>
+
+
+E-mail:
+<input type="text"> <br><br>
+
+Password:
+<input type="password"> <br><br>
+
+please choose a file
+<input type="file"> <br><br>
+
+<br><br>
+
+
+<center>
+    <input type ="Submit">
+    <input type ="Reset">
+</center>
+
+
+
+</body>
